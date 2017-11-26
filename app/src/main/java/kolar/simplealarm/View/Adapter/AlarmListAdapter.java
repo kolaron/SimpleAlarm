2acaafb8fd86dde0334b79ecf23cf155292e2fe9
Ondřej Kolář <kolar.ondra@gmail.com>
2017-11-26 08:45:55 +0100

    Change GUI

--- app/src/main/java/kolar/simplealarm/View/Adapter/AlarmListAdapter.java
+++ app/src/main/java/kolar/simplealarm/View/Adapter/AlarmListAdapter.java
@@ -15,11 +15,16 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import java.util.ArrayList;
+
+import kolar.simplealarm.Model.AlarmClass;
 import kolar.simplealarm.R;
 
 /**
@@ -28,211 +33,100 @@ import kolar.simplealarm.R;
 
 public class AlarmListAdapter extends RecyclerView.Adapter<AlarmListAdapter.ViewHolder> {
 
+    private ArrayList<AlarmClass> list = new ArrayList<>();
 
     public AlarmListAdapter() {
-
+        list.add(null);
+        list.add(null);
+        list.add(null);
+        list.add(null);
+        list.add(null);
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View view = layoutInflater.inflate(R.layout.alarmlistitem, parent, false);
+        View view = layoutInflater.inflate(R.layout.alarmlist_item, parent, false);
         return new ViewHolder(view, parent.getContext());
     }
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
-        holder.buttonExpand.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onExpandButtonClick(holder.context, holder.expandableLayout, holder.buttonExpand, holder.textView_dayRepeat, holder.linearLayout_remove, holder.separator_bott);
-            }
-        });
-        holder.textView_time.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    String[] time = holder.textView_time.getText().toString().split(":");
-                    TimePickerDialog mTimePicker;
-                    mTimePicker = new TimePickerDialog(holder.context, new TimePickerDialog.OnTimeSetListener() {
-                        @Override
-                        public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                            holder.textView_time.setText(selectedHour + ":" + selectedMinute);
-                        }
-                    }, Integer.parseInt(time[0]), Integer.parseInt(time[1]), true);//Yes 24 hour time
-                    mTimePicker.show();
-                } catch (Exception ex) {
-
-                }
-
-            }
-        });
-        holder.linearLayout_remove.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-// TODO: 25.11.2017 odstranit udaj
-            }
-        });
-        holder.button_dayM.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onDayClick(holder.context, holder.text_dayM);
-            }
-        });
-
-        holder.button_dayTu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onDayClick(holder.context, holder.text_dayTu);
-            }
-        });
-        holder.button_dayF.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onDayClick(holder.context, holder.text_dayF);
-            }
-        });
-        holder.button_daySa.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onDayClick(holder.context, holder.text_daySa);
-            }
-        });
-        holder.button_daySu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onDayClick(holder.context, holder.text_daySu);
-            }
-        });
-        holder.button_dayTh.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onDayClick(holder.context, holder.text_dayTh);
-            }
-        });
-        holder.button_dayW.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onDayClick(holder.context, holder.text_dayW);
-            }
-        });
-
-        holder.checkbox_repeat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        holder.checkbox_defer.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                onCheckBoxChange(holder.context, isChecked, holder.linearLayout_days);
-                holder.linearLayout_days.clearAnimation();
-                if(isChecked){
-                    holder.linearLayout_days.setVisibility(View.VISIBLE);
-                }else{
-                    holder.linearLayout_days.setVisibility(View.GONE);
+                if (isChecked) {
+                    holder.linearLayout_expand.setVisibility(View.VISIBLE);
+                } else {
+                    holder.linearLayout_expand.startAnimation(AnimationUtils.loadAnimation(holder.context, R.anim.fade_in));
+                    holder.linearLayout_expand.setVisibility(View.GONE);
                 }
+            }
+        });
+
+        holder.relativeLayout_expandButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.expandeLayout(holder.linearLayout_expandMain, holder.relativeLayout_expandButton);
             }
         });
     }
 
     @Override
     public int getItemCount() {
-        return 10;
+        return list.size();
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
-
-        TextView textView_time, textView_dayRepeat;
-        Switch switch_activate;
-
-        LinearLayout expandableLayout, linearLayout_days, linearLayout_remove;
-        CheckBox checkbox_repeat;
-        TextView text_dayM, text_dayTu, text_dayW, text_dayTh, text_dayF, text_daySa, text_daySu;
-        RelativeLayout buttonExpand, button_dayM, button_dayTu, button_dayW, button_dayTh, button_dayF, button_daySa, button_daySu;
-
-        View separator_bott;
-
         Context context;
+        TextView textView_time, textView_properties;
+        Switch switch_activate;
+        CheckBox checkbox_repeat, checkbox_defer;
+        RadioGroup radioGroup_defer;
+        RadioButton radioBttn5, radioBttn10, radioBttn15, radioBttn30;
+        LinearLayout linearLayout_expand, linearLayout_expandMain;
+
+        RelativeLayout relativeLayout_expandButton;
+        ImageView imageView_remove;
 
         public ViewHolder(View itemView, Context context) {
             super(itemView);
             this.context = context;
             textView_time = itemView.findViewById(R.id.textView_time);
             switch_activate = itemView.findViewById(R.id.switch_activate);
-
-            expandableLayout = itemView.findViewById(R.id.expandableLayout);
             checkbox_repeat = itemView.findViewById(R.id.checkbox_repeat);
-            linearLayout_days = itemView.findViewById(R.id.linearLayout_days);
-
-//            days
-            button_dayM = itemView.findViewById(R.id.button_dayM);
-            button_dayTu = itemView.findViewById(R.id.button_dayTu);
-            button_dayW = itemView.findViewById(R.id.button_dayW);
-            button_dayTh = itemView.findViewById(R.id.button_dayTh);
-            button_dayF = itemView.findViewById(R.id.button_dayF);
-            button_daySa = itemView.findViewById(R.id.button_daySa);
-            button_daySu = itemView.findViewById(R.id.button_daySu);
-
-            text_dayM = itemView.findViewById(R.id.text_dayM);
-            text_dayTu = itemView.findViewById(R.id.text_dayTu);
-            text_dayW = itemView.findViewById(R.id.text_dayW);
-            text_dayTh = itemView.findViewById(R.id.text_dayTh);
-            text_dayF = itemView.findViewById(R.id.text_dayF);
-            text_daySa = itemView.findViewById(R.id.text_daySa);
-            text_daySu = itemView.findViewById(R.id.text_daySu);
-
-            textView_dayRepeat = itemView.findViewById(R.id.textView_dayRepeat);
-            linearLayout_remove = itemView.findViewById(R.id.linearLayout_remove);
-            buttonExpand = itemView.findViewById(R.id.buttonExpand);
-
-            separator_bott = itemView.findViewById(R.id.separator_bott);
+            checkbox_defer = itemView.findViewById(R.id.checkbox_defer);
+            radioGroup_defer = itemView.findViewById(R.id.radioGroup_defer);
+            radioBttn5 = itemView.findViewById(R.id.radioBttn5);
+            radioBttn10 = itemView.findViewById(R.id.radioBttn10);
+            radioBttn15 = itemView.findViewById(R.id.radioBttn15);
+            radioBttn30 = itemView.findViewById(R.id.radioBttn30);
+            linearLayout_expand = itemView.findViewById(R.id.linearLayout_expand);
+            relativeLayout_expandButton = itemView.findViewById(R.id.relativeLayout_expandButton);
+            textView_properties = itemView.findViewById(R.id.textView_properties);
+            imageView_remove = itemView.findViewById(R.id.imageView_remove);
+            linearLayout_expandMain = itemView.findViewById(R.id.linearLayout_expandMain);
         }
-    }
 
-    private void onExpandButtonClick(Context context, final LinearLayout expandableLayout, final RelativeLayout buttonExpand, final TextView textView_dayRepeat, final LinearLayout linearLayout_remove, final View separator) {
-        Animation animFadeOut = AnimationUtils.loadAnimation(context, R.anim.fade_out);
-        Animation animFadeIn = AnimationUtils.loadAnimation(context, R.anim.fade_in);
-        if (expandableLayout.getVisibility() == View.GONE) {
-            createRotateAnimator(buttonExpand, 0f, 180f).start();
-            expandableLayout.setVisibility(View.VISIBLE);
-            textView_dayRepeat.setVisibility(View.GONE);
-            linearLayout_remove.setVisibility(View.VISIBLE);
-            separator.setVisibility(View.GONE);
-            expandableLayout.clearAnimation();
-            expandableLayout.startAnimation(animFadeIn);
-        } else {
-            expandableLayout.setVisibility(View.GONE);
-            textView_dayRepeat.setVisibility(View.VISIBLE);
-            linearLayout_remove.setVisibility(View.GONE);
-            separator.setVisibility(View.VISIBLE);
-            createRotateAnimator(buttonExpand, 180f, 0f).start();
-            expandableLayout.clearAnimation();
-            expandableLayout.startAnimation(animFadeOut);
+        private void expandeLayout(LinearLayout linearLayout_expandMain, RelativeLayout expandButton) {
+            Animation animFadeOut = AnimationUtils.loadAnimation(context, R.anim.fade_out);
+            Animation animFadeIn = AnimationUtils.loadAnimation(context, R.anim.fade_in);
+            if (linearLayout_expandMain.getVisibility() == View.GONE) {
+                linearLayout_expandMain.startAnimation(animFadeIn);
+                createRotateAnimator(expandButton, 0f, 180f).start();
+                linearLayout_expandMain.setVisibility(View.VISIBLE);
+            } else {
+                linearLayout_expandMain.startAnimation(animFadeOut);
+                createRotateAnimator(expandButton, 180f, 0f).start();
+                linearLayout_expandMain.setVisibility(View.GONE);
+            }
         }
-    }
 
-    private void onCheckBoxChange(Context context, boolean isChecked, LinearLayout linearLayout_days) {
-        if (isChecked) {
-            linearLayout_days.setVisibility(View.VISIBLE);
-        } else {
-            linearLayout_days.setVisibility(View.GONE);
+        private ObjectAnimator createRotateAnimator(final View target, final float from, final float to) {
+            ObjectAnimator animator = ObjectAnimator.ofFloat(target, "rotation", from, to);
+            animator.setDuration(300);
+            animator.setInterpolator(new LinearInterpolator());
+            return animator;
         }
-    }
-
-    private void onDayClick(Context context, TextView day) {
-        Animation animFadeOut = AnimationUtils.loadAnimation(context, android.R.anim.fade_out);
-        Animation animFadeIn = AnimationUtils.loadAnimation(context, android.R.anim.fade_in);
-        if (day.getBackground() == null) {
-            day.startAnimation(animFadeOut);
-            day.setBackgroundResource(R.drawable.circle_day);
-            day.setTextColor(Color.BLACK);
-        } else {
-            day.startAnimation(animFadeIn);
-            day.setBackground(null);
-            day.setTextColor(Color.WHITE);
-        }
-    }
-
-    private ObjectAnimator createRotateAnimator(final View target, final float from, final float to) {
-        ObjectAnimator animator = ObjectAnimator.ofFloat(target, "rotation", from, to);
-        animator.setDuration(300);
-        animator.setInterpolator(new LinearInterpolator());
-        return animator;
     }
 }
